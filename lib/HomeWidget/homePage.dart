6ef5a4a7b9a756972a4de2420489c05b5d8cf257
Abel Shedrack Nicholas <shedrackejeh458@gmail.com>
2020-09-28 12:54:09 +0100

    done

--- lib/HomeWidget/homePage.dart
+++ lib/HomeWidget/homePage.dart
@@ -1,1 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('PitchMe'),
+        backgroundColor: Colors.pinkAccent[200],
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.menu),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(FontAwesomeIcons.camera),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.more_vert),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
